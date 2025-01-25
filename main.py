bc6246620af149007f84ff61da116c767b127959
Amit <gaikwadamit007@gmail.com>
2025-01-26 02:40:46 +0530

    dvc final

---
+++ main.py
@@ -0,0 +1,7 @@
+# import os
+
+# os.system("python src/data_ingestion.py")    
+# os.system("python src/data_preprocessing.py")
+# os.system("python src/feature_engineering.py")
+# os.system("python src/model_building.py")
+# os.system("python src/model_evaluation.py")
