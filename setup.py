80bc312440f3d797c3a35cc88986d21eef4e7c9d
Amit <gaikwadamit007@gmail.com>
2025-01-26 01:15:51 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='MlPipeline',
+    packages=find_packages(),
+    version='0.0.0',
+    description='ML Pipeline with DVC',
+    author='Amit Gaikwad',
+    author_email="amitgaikwadds@gmailcom",
+    license='MIT',
+    install_requires=[]
+)
+
